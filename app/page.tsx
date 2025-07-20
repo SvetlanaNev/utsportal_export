19bb5db491dfe469b8e8e2a9d7d011f30b979009
Svetlana Nevskaya <svetlana.nevskaya@uts.edu.au>
2025-07-21 08:50:35 +1000

    chore: add diagnostic test for Vercel build

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import { LoginForm } from "@/components/login-form";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-100">
-      <LoginForm />
+    <main className="flex h-screen w-screen items-center justify-center bg-red-500">
+      <h1 className="text-4xl font-bold text-white">
+        If you see this red screen, the build process is working.
+      </h1>
     </main>
   );
 }
